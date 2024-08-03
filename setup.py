09eb3d2614215884a5d73b4239be0155773afee9
Daisy Lal <116365685+dml2611@users.noreply.github.com>
2024-08-03 19:54:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='practice_math',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    version='0.0.1',
+    author='Daisy Lal',
+    author_email='daisylal26@gmail.com',
     description='Fun Maths',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
